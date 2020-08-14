bc47455c1d03cfd3b46f050dfeedf54932e46fdb
Jose Garcia <eng.josegc@gmail.com>
2020-08-14 10:51:15 -0500

    Add services practice finished

--- app/src/app/hero.service.ts
+++ app/src/app/hero.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Hero } from "./heroes/hero";
 import { HEROES } from "./heroes/mock-heroes";
+import { Observable, of } from 'rxjs'; // To simulate requests to server
+import { MessageService } from './message.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
   }
 }
--- app/src/app/heroes/heroes.component.ts
+++ app/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 // import { HEROES } from './mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,18 +15,25 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
 
   // Injection of the service
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private heroService: HeroService, 
+    private messageService: MessageService
+    ) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   // Responds to click event in the template
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   // Retrieves all heroes from the service injected
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+      // Critical: asynchronys response from the server
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
