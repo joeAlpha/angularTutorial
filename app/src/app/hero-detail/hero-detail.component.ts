543e051c62d88263e0949df99878f610e15066de
Jose Garcia <eng.josegc@gmail.com>
2020-08-25 09:18:26 -0500

    Practice of routing done but the hero list dont show details

--- app/src/app/hero-detail/hero-detail.component.ts
+++ app/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Hero} from '../heroes/hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,9 +15,28 @@ export class HeroDetailComponent implements OnInit {
   // input property.
   @Input() hero: Hero;
 
-  constructor() { }
+  // route: information about the route and url for this instance
+  // location: service for interact with the browser
+  constructor(
+    private route: ActivatedRoute, 
+    private heroService: HeroService,
+    private location: Location) { }
 
-  ngOnInit(): void {
-  }
+    // When the component is instantiate gets the heroes from the server
+    ngOnInit(): void {
+      this.getHero();
+    }
+    
+    getHero(): void {
+      // Extracts the id from the url (a dictionary structure)
+      // and the + converts it into a number type.
+      const id = +this.route.snapshot.paramMap.get('id');
+      this.heroService.getHero(id)
+        .subscribe(hero => this.hero = hero);
+    }
+
+    goBack(): void {
+      this.location.back();
+    }
 
 }
--- app/src/app/hero.service.ts
+++ app/src/app/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Hero } from "./heroes/hero";
-import { HEROES } from "./heroes/mock-heroes";
+import { Hero } from './heroes/hero';
+import { HEROES } from './heroes/mock-heroes';
 import { Observable, of } from 'rxjs'; // To simulate requests to server
 import { MessageService } from './message.service';
 
@@ -12,7 +12,14 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
